ff3555da323fe008ab1b28ece5de1b1ddd44d910
Rudolf Farkas <rudi.farkas@gmail.com>
2017-10-10 18:56:28 +0200

    Added code to close the main window

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const {ipcRenderer} = require('electron')
+
+var closeEl = document.querySelector('.close');
+closeEl.addEventListener('click', function () {
+    ipcRenderer.send('close-main-window');
+});
+
 var soundButtons = document.querySelectorAll('.button-sound');
 
 for (var i = 0; i < soundButtons.length; i++) {
--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-'use strict';
+'use strict'
 
 var app = require('electron').app
 var BrowserWindow = require('electron').BrowserWindow
-
-
+const {ipcMain} = require('electron')
 var mainWindow = null;
+
+ipcMain.on('close-main-window', (event, arg) => {
+    app.quit();
+});
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
-    	frame: false,
-    	resizable: false,
+        frame: false,
+        resizable: false,
         height: 700,
         width: 368
     });
 
-	mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 });
