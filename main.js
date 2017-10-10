900098c088669be6f92cc88f3b4ac2ed9cee8521
Rudolf Farkas <rudi.farkas@gmail.com>
2017-10-10 22:44:12 +0200

    added configuration + settings for global shortcuts

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ var app = require('electron').app
 var BrowserWindow = require('electron').BrowserWindow
 var mainWindow = null;
 const {globalShortcut} = require('electron');
+var configuration = require('./configuration');
 
 app.on('ready', function() {
+    if (!configuration.readSettings('shortcutKeys')) {
+        configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
+    }
+
     mainWindow = new BrowserWindow({
         frame: false,
         resizable: false,
@@ -15,12 +20,8 @@ app.on('ready', function() {
 
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 
-    globalShortcut.register('ctrl+shift+1', function () {
-        mainWindow.webContents.send('global-shortcut', 0);
-    });
-    globalShortcut.register('ctrl+shift+2', function () {
-        mainWindow.webContents.send('global-shortcut', 1);
-    });
+    setGlobalShortcuts()
+
 });
 
 const {ipcMain} = require('electron')
@@ -55,3 +56,20 @@ ipcMain.on('close-settings-window', function () {
     }
 });
 
+function setGlobalShortcuts() {
+    globalShortcut.unregisterAll();
+
+    var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
+    var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
+
+    globalShortcut.register(shortcutPrefix + '1', function () {
+        mainWindow.webContents.send('global-shortcut', 0);
+    });
+    globalShortcut.register(shortcutPrefix + '2', function () {
+        mainWindow.webContents.send('global-shortcut', 1);
+    });
+}
+
+ipcMain.on('set-global-shortcuts', function () {
+    setGlobalShortcuts();
+});
